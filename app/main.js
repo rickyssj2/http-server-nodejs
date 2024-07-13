4f4ac2e5d4beb6b52bbfc7b6bfeb30baa41d6f91
Aryan A. Pandey <aryan.a.pandey@accenture.com>
2024-07-13 18:00:07 +0900

    stage 3.2

--- app/main.js
+++ app/main.js
@@ -4,18 +4,13 @@ const server = net.createServer((socket) => {
     socket.write("HTTP/1.1 200 OK\r\n\r\n");
     socket.on("data", (data)=>{
         const req = data.toString()
-        console.log('Request received',req)
         if (req.startsWith('GET / ')) {
             socket.write("HTTP/1.1 200 OK\r\n\r\n")
         } else {
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+            socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
         }
     });
-    socket.on("error",(error) =>{
-        console.log("ERROR! : ",error)
-    });
     socket.on("close", () => {
-        console.log('Server closed!')
         socket.end();
     });
 });
