42e62189246e573be6cdae7bfc65320ee70675e3
Aryan A. Pandey <aryan.a.pandey@accenture.com>
2024-07-18 10:34:58 +0900

    stage 6

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require('node:fs');
 
 const server = net.createServer((socket) => {
     socket.on("data", (data)=>{
@@ -12,10 +13,21 @@ const server = net.createServer((socket) => {
         } else if (startLine.split('/')[1].split(' ')[0] == 'user-agent') {
             _headersAndBody.forEach(line => {
                 if(line.startsWith('User-Agent:')){
-                    value = line.split(': ')[1];
+                    let value = line.split(': ')[1];
                     socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${value.length}\r\n\r\n${value}`)
                 }
             });
+        }  else if (startLine.split('/')[1].split(' ')[0] == 'files') {
+            loc = startLine.split('/')[1].split(' ')[1] || ''
+            let value;            
+            try {
+                const data = fs.readFileSync(loc, 'utf8');
+                value = data;
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${value.length}\r\n\r\n${value}`)
+            } catch (err) {
+                console.log('File not found!')
+                socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
+            }          
         }
         else {
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
