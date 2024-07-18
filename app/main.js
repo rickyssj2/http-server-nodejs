3f7f43c8800916052d3a3c5215e007e2cea928a0
Aryan A. Pandey <aryan.a.pandey@accenture.com>
2024-07-18 11:12:06 +0900

    stage 6.1

--- app/main.js
+++ app/main.js
@@ -1,11 +1,15 @@
 const net = require("net");
 const fs = require('node:fs');
+const path = require('path')
 
 const server = net.createServer((socket) => {
-    socket.on("data", (data)=>{
+    socket.on("data", (data)=>{        
         const req = data.toString()
         const [startLine, ..._headersAndBody] = req.split('\r\n');
-        const [_method, path, _version] = startLine.split(' ');
+        const [_method, resourcePath, _version] = startLine.split(' ');
+        const flags = process.argv.slice(2);
+        const directory = flags.find((_, index) => flags[index - 1] == "--directory");  
+
         if (startLine.startsWith('GET / ')) {
             socket.write("HTTP/1.1 200 OK\r\n\r\n")
         } else if (startLine.split('/')[1] == 'echo') {
@@ -17,23 +21,24 @@ const server = net.createServer((socket) => {
                     socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${value.length}\r\n\r\n${value}`)
                 }
             });
-        }  else if (startLine.split('/')[1].split(' ')[0] == 'files') {
-            loc = startLine.split('/')[1].split(' ')[1] || ''
-            let value;            
-            try {
-                const data = fs.readFileSync(loc, 'utf8');
-                value = data;
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${value.length}\r\n\r\n${value}`)
-            } catch (err) {
-                console.log('File not found!')
-                socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
-            }          
+        }  else if(startLine.startsWith("/files/")){
+            const filePath = startLine.slice(7);
+            if(!fs.existsSync(directory + filePath)){
+                socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+                socket.end();
+                return;
+            }
+            const file = fs.readFileSync(directory + filePath);
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`);
         }
         else {
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
         }
         socket.end();
     });
+    socket.on('error',()=>{
+        console.log("Socket Error!!")
+    })
     socket.on("close", () => {
         socket.end();
     });
